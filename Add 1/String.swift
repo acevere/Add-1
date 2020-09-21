53a4dd4492e562739b5e004b840ef71321d567e3
acevere <assver.ay@gmail.com>
2020-09-21 11:01:46 -0400

    Add files

---
+++ Add 1/String.swift
@@ -0,0 +1,33 @@
+//
+//  String.swift
+//  Add 1
+//
+//  Created by Alice Ye on 2020-06-17.
+//  Copyright © 2020 Alice Ye. All rights reserved.
+//
+
+import Foundation
+
+extension String
+{
+    static func randomNumber(length: Int) -> String
+    {
+        var result = ""
+
+        for _ in 0..<length {
+            let digit = Int.random(in: 0...9)
+            result += "\(digit)"
+        }
+
+        return result
+    }
+    
+    func integer(at n: Int) -> Int
+    {
+        let index = self.index(self.startIndex, offsetBy: n)
+
+        return self[index].wholeNumberValue ?? 0
+    }
+}
+
+
